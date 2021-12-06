d0b54fa8ae704cfe6dd08e60f27fc03f3d4ad264
Melvin Adu <contactmelvinadu@gmail.com>
2021-12-06 23:22:33 +0000

    add view page for URL database

--- express_server.js
+++ express_server.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = 8080;
 
 const urlDatabase = {
-  "0": "www.lighthouselabs.ca",
-  "1": "www.google.com"
+  "0213213": "www.lighthouselabs.ca",
+  "1213213": "www.google.com"
 };
 
 app.set('view engine', 'ejs')
@@ -19,6 +19,11 @@ app.get ('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
+});
+
+app.get('/urls', (req,res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.listen(PORT, () => {
