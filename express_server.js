e31363fd527caf201e09eec22e3db0894865108e
Melvin Adu <contactmelvinadu@gmail.com>
2021-12-07 20:36:39 +0000

    add edit button to index page and post request to update the long URL of any key in the database

--- express_server.js
+++ express_server.js
@@ -69,49 +69,13 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect(`/urls`);         // Respond redirect
 });
 
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  urlDatabase[id] = req.body.longURL;
+
+  res.redirect(`/urls`);         // Respond redirect
+});
+
 app.listen(PORT, () => {
   console.log(`Tinyapp is listening on port ${PORT}.`);
 });
-
-/*
-const express = require('express');
-const app = express();
-const PORT = 8080;
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const urlDatabase = {
-  "b2xVn2": 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com',
-};
-
-app.set('view engine', 'ejs');
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
-});
-
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-});
-
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('urls_show', templateVars);
-});
-
-app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('Ok');
-});
-
-app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-*/
