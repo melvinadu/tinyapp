8b93ae363b958b8ad747d61fe404a25109fad1fa
Melvin Adu <contactmelvinadu@gmail.com>
2021-12-07 19:32:27 +0000

    add delete button to index page and post response to server

--- express_server.js
+++ express_server.js
@@ -17,10 +17,6 @@ function generateRandomString() {
 
 app.set('view engine', 'ejs')
 
-app.listen(PORT, () => {
-  console.log(`Tinyapp is listening on port ${PORT}.`);
-});
-
 app.get("/", (req, res,) => {
   res.send("Welcome to Tinyapp");
 });
@@ -53,6 +49,11 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   let newID = generateRandomString();
@@ -60,11 +61,17 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newID}`);         // Respond redirect
 });
 
-app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+
+  delete urlDatabase[shortURL];
+
+  res.redirect(`/urls`);         // Respond redirect
 });
 
+app.listen(PORT, () => {
+  console.log(`Tinyapp is listening on port ${PORT}.`);
+});
 
 /*
 const express = require('express');
