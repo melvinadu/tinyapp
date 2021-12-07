cf7e8262ec62a696e53279f4d00021d6c14da2c4
Melvin Adu <contactmelvinadu@gmail.com>
2021-12-07 01:26:54 +0000

    add body parser for post requests and generate random string to serve as shortURL

--- express_server.js
+++ express_server.js
@@ -7,6 +7,15 @@ const urlDatabase = {
   "9sm5xK": "www.google.com"
 };
 
+function generateRandomString() {
+    return Math.random().toString(20).substr(2, 6)
+};
+
+console.log(generateRandomString());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs')
 
 app.get("/", (req, res,) => {
@@ -33,6 +42,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
